93531dea639bb191ccc5cdd6a6288b51e6f75948
Landon Dorrier <landon.dorrier@gmail.com>
2019-07-26 11:26:44 -0400

    Update core-functionality.php

--- core-functionality.php
+++ core-functionality.php
@@ -21,116 +21,6 @@
  */
 
 
-if ( ! function_exists( 'core_functionality_setup' ) ) :
-	/**
-	 * Sets up theme defaults and registers support for various WordPress features.
-	 *
-	 * Note that this function is hooked into the after_setup_theme hook, which
-	 * runs before the init hook. The init hook is too late for some features, such
-	 * as indicating support for post thumbnails.
-	 */
-	function core_functionality_setup() {
-		
-		// Add support for Block Styles.
-		add_theme_support( 'wp-block-styles' );
-
-		// Add support for full and wide align images.
-		add_theme_support( 'align-wide' );
-
-		// Add support for editor styles.
-		add_theme_support( 'editor-styles' );
-
-		// Enqueue editor styles.
-		add_editor_style( 'css/editor-style.css' );
-
-		// Add custom editor font sizes.
-		add_theme_support(
-			'editor-font-sizes',
-			array(
-				array(
-					'name'      => __( 'Small' ),
-					'shortName' => __( 'S' ),
-					'size'      => 14,
-					'slug'      => 'small',
-				),
-				array(
-					'name'      => __( 'Normal' ),
-					'shortName' => __( 'N' ),
-					'size'      => 18,
-					'slug'      => 'normal',
-				),
-				array(
-					'name'      => __( 'Large' ),
-					'shortName' => __( 'L' ),
-					'size'      => 36,
-					'slug'      => 'large',
-				),
-				array(
-					'name'      => __( 'Huge' ),
-					'shortName' => __( 'XL' ),
-					'size'      => 54,
-					'slug'      => 'huge',
-				),
-			)
-		);
-
-		// Editor color palette.
-		add_theme_support(
-			'editor-color-palette',
-			array(
-				array(
-					'name'  => __( 'Black' ),
-					'slug'  => 'black',
-					'color' => '#000',
-				),
-				array(
-					'name'  => __( 'White' ),
-					'slug'  => 'white',
-					'color' => '#fff',
-				),
-			)
-		);
-		
-		// Disable the custom color picker
-		add_theme_support( 'disable-custom-colors' );
-
-		// Add support for responsive embedded content.
-		add_theme_support( 'responsive-embeds' );
-	}
-endif;
-add_action( 'after_setup_theme', 'core_functionality_setup' );
-
-
-/**
- * Enque script in the WP Editor
- */
-function editor_script_enqueue($hook) {
-    // Only add to the post.php admin page.
-    // See WP docs.
-    if ('post.php' !== $hook) {
-        return;
-    }
-    wp_enqueue_script('theme-editor-js', get_template_directory_uri() . '/js/editor.js', array(), '', true);
-}
-add_action('admin_enqueue_scripts', 'editor_script_enqueue');
-
-
-/**
- * Set the content width wider in pixels, based on the theme's design and stylesheet.
- *
- * Priority 0 to make it available to lower priority callbacks.
- *
- * @global int $content_width
- */
-function core_wider_content_width() {
-	// This variable is intended to be overruled from themes.
-	// Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
-	// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
-	$GLOBALS['content_width'] = apply_filters( 'core_wider_content_width', 768 );
-}
-add_action( 'after_setup_theme', 'core_wider_content_width', 0 );
-
-
 /**
  * Disable the emoji's
  */
@@ -162,40 +52,9 @@ function disable_emojis_tinymce( $plugins ) {
 }
 
 
-/**
- * Custom Image Sizes
- */
-//add_image_size( 'medium', 960, 540, true ); // width, height, crop
-
-
-/**
- * Custom WP Login Logo
- */
-function core_login_logo() { ?>
-    <style type="text/css">
-        #login h1 a, .login h1 a {
-            background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/logo.png);
-			height: 110px;
-			width: 320px;
-			background-repeat: no-repeat;
-			padding-bottom: 40px;
-			background-size: contain;
-			margin-bottom: 40px;
-        }
-    </style>
-<?php }
-add_action( 'login_enqueue_scripts', 'core_login_logo' );
-
-function core_login_logo_url() {
-    return home_url();
-}
-add_filter( 'login_headerurl', 'core_login_logo_url' );
-
-function core_login_logo_url_title() {
-    return 'Organization Name';
-}
-add_filter( 'login_headertitle', 'core_login_logo_url_title' );
-
+/*******************************************
+ * BEGIN ACF *
+ ******************************************/
 
 /**
  * ACF Theme Options page
@@ -209,44 +68,93 @@ if( function_exists('acf_add_options_page') ) {
 	));
 }
 
+/********************************************************************
+ * ACF Blocks (PLACEHOLDER)
+ *******************************************************************/
+/*
+function pha_register_blocks() {
+	
+	// check function exists.
+    if( function_exists('acf_register_block') ) {
+		
+		// register a Properties block
+		acf_register_block(array(
+			'name'				=> 'properties',
+			'title'				=> __('Properties'),
+			'description'		=> __('A block that display your properties.'),
+			'icon'				=> 'admin-multisite',
+			'keywords'			=> array( 'property', 'housing', 'rentals' ),
+			'mode' 				=> 'preview',
+			'category'			=> 'widgets',
+			'render_template'   => 'blocks/properties/properties.php',
+		));
+    }
+}
+add_action('acf/init', 'pha_register_blocks' );
+*/
 
-/* Remove "Category:" and "Tag:" from get_the_archive_title() */
-add_filter( 'get_the_archive_title', function ($title) {
-	if ( is_category() ) {
-		$title = single_cat_title( '', false );
-	} 
-	elseif ( is_tag() ) {
-		$title = single_tag_title( '', false );
-	} 
-	elseif ( is_author() ) {
-		$title = '<span class="vcard">' . get_the_author() . '</span>' ;
-	}
-	elseif ( is_year() ) {
-        $title = get_the_date( _x( 'Y', 'yearly archives date format' ) );
-	}
-	return $title;
-});
-
+/********************************************************************
+ * Custom Post Types
+ *******************************************************************/
+/**
+ * Custom Post Type Taxonomy - Properties
+ */
+/*	
+function create_properties_taxonomies() {
+    register_taxonomy(
+		'properties_category',
+		'properties',
+		array(
+			'labels' => array(
+				'name' => 'Properties Categories',
+				'add_new_item' => 'Add New Category',
+				'new_item_name' => 'New Category'
+			),
+			'show_ui' => true,
+			'show_tagcloud' => false,
+			'show_admin_column' => true,
+			'hierarchical' => true,
+			'show_in_rest' => true,
+		)
+	);
+}
+add_action('init', 'create_properties_taxonomies');
 
 /**
- * Filter the excerpt length to 20 words.
- *
- * @param int $length Excerpt length.
- * @return int (Maybe) modified excerpt length.
+ * Adds Custom Post Type - Properties
  */
-function core_short_excerpt_length( $length ) {
-    return 30;
-}
-add_filter( 'excerpt_length', 'core_short_excerpt_length', 999 );
 
-
-/**
- * Filter the excerpt "read more" string.
- *
- * @param string $more "Read more" excerpt string.
- * @return string (Maybe) modified "read more" excerpt string.
- */
-function core_excerpt_more( $more ) {
-    return '...';
+/* 
+add_action('init', 'properties_register');
+function properties_register() {
+	$labels = array(
+		'name' => _x('Properties', 'post type general name'),
+		'singular_name' => _x('Property', 'post type singular name'),
+		'add_new' => _x('Add New', 'Property'),
+		'add_new_item' => __('Add New Property'),
+		'edit_item' => __('Edit Property'),
+		'new_item' => __('New Property'),
+		'all_items' => __('All Properties'),
+		'view_item' => __('View Property'),
+		'search_items' => __('Search Properties'),
+		'not_found' =>  __('Nothing found'),
+		'not_found_in_trash' => __('Nothing found in Trash'),
+		'parent_item_colon' => ''
+	);
+	$args = array(
+		'labels' => $labels,
+		'public' => true,
+		'publicly_queryable' => false,
+		'show_ui' => true,
+		'query_var' => true,
+		'show_in_nav_menus' => false,
+		'hierarchical' => true,
+		'menu_position' => null,
+		'supports' => array('title', 'thumbnail', 'page-attributes'),
+		'has_archive' => false,
+		'show_in_rest' => true,
+		'menu_icon' => 'dashicons-admin-multisite',
+	  );  
+	register_post_type( 'properties' , $args );
 }
-add_filter( 'excerpt_more', 'core_excerpt_more' );
+*/
