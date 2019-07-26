a143f1cdb52ca333f448976b71c90159a0b39cf0
Landon Dorrier <landon.dorrier@gmail.com>
2019-07-26 10:04:13 -0400

    Create core-functionality.php

---
+++ core-functionality.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin Name: Core Functionality
+ * Description: This contains all your site's core functionality so that it is theme independent. <strong>It should always be activated</strong>.
+ * Version:     1.0
+ * Author:      Landon Dorrier
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License version 2, as published by the
+ * Free Software Foundation.  You may NOT assume that you can use any other
+ * version of the GPL.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.
+ *
+ * @package    CoreFunctionality
+ * @since      1.0.0
+ * @copyright  Copyright (c) 2019, Landon Dorrier
+ * @license    GPL-2.0+
+ */
